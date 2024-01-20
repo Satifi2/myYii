d013763e88a4c753551cf9c5c8241bd12a5bcd85
du-yixing <2602521797@qq.com>
2024-01-20 23:26:27 +0800

    form generator

---
+++ backend/views/_form.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var common\models\User $model */
+/** @var ActiveForm $form */
+?>
+<div class="form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+        <?= $form->field($model, 'status') ?>
+    
+        <div class="form-group">
+            <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
+        </div>
+    <?php ActiveForm::end(); ?>
+
+</div><!-- form -->
